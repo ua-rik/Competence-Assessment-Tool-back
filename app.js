b67796fa322b3a3f47b38b736328dc0af4be6e2c
Yaroslav Davydchuk <d.yarik@gmail.com>
2024-04-28 21:33:37 +0100

    add .env

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 
-const participantRouter = require('./routes/participantRoutes')
+const participantRouter = require('./routes/participantRoutes');
 
 const app = express();
 
 
 // middlewares
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const dotenv = require('dotenv');
+// має бути визначено до декларації app
+dotenv.config({ path: './config.env' });
+
 const app = require('./app')
 
-const port = 3000;
+
+
+console.log('🧑‍💻️Enviroment:', app.get('env'))
+console.log(process.env)
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
